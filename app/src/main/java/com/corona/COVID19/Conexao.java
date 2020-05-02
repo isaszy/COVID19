d4d42975ecd6af1518f5c9b1f6259537fa26f8ac
anderson361163 <anderson361163@gmail.com>
2020-05-02 15:04:05 -0300

    Criação da tabela do SQLite e as variaveis de conexao com o banco

--- app/src/main/java/com/corona/COVID19/Conexao.java
+++ app/src/main/java/com/corona/COVID19/Conexao.java
@@ -7,12 +7,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class Conexao extends SQLiteOpenHelper {
-    public Conexao(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    private static final String banco = "bancoUser";
+    private static final int version = 1;
+
+
+    public Conexao(@Nullable Context context) {
+        super(context, banco, null, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE DadosUsuario(id integer primary key autoincrement," +
+                "nome varchar(70)," +
+                "Telefone varchar(12)," +
+                "Idade varchar(3)," +
+                "N_SUS varchar(25)," +
+                "Diabete varchar(3)," +
+                "Senha varchar(20))");
 
     }
 
